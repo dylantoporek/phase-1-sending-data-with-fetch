3ae8c6aade0042947cb146d2f08cc4dbf72aa568
DylanToporek <dylantoporek@gmail.com>
2021-10-02 13:38:26 -0400

    Done.

--- index.js
+++ index.js
@@ -23,21 +23,7 @@ function submitData(name, email){
           let h1 = document.createElement('h1')
         h1.innerHTML = `${object.id}`;
         body.appendChild(h1);
+      }).catch(function(error){
+        document.body.innerHTML = error.message;
       })
-      
-      .catch(function(error){
-        
-          let h1 = document.createElement('h1')
-          document.body.innerHTML = error.message;
-          console.log(error.message, 'error message')
-          document.body.append(h1);
-          console.log(document.body.innerHTML, 'DOC BODY')
-        // let body = document.querySelector('body');
-        // let p = document.createElement('p');
-        // p.innerText = error.message;
-        // body.innerHTML = 'Unauthorized Access';
-        
-    })
-
-
-}
+  }
