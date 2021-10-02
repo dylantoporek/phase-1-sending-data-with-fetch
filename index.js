0e81b15472d8c9d75414e4009feb7d5698ea9b7b
DylanToporek <dylantoporek@gmail.com>
2021-10-02 13:40:02 -0400

    commit tree f4f21e0ffc9b14c572e9f6b5852879e0b8c0000a

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function submitData(name, email){
         body: JSON.stringify(userData),
       };
 
-      fetch("http://localhost:3000/users", configurationObject)
+      return fetch("http://localhost:3000/users", configurationObject)
       .then(function (response) {
         return response.json();
       })
@@ -27,3 +27,26 @@ function submitData(name, email){
         document.body.innerHTML = error.message;
       })
   }
+
+  // function submitData( name, email ) {
+  //   return fetch( 'http://localhost:3000/users', {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         "Accept": "application/json"
+  //       },
+  //       body: JSON.stringify( {
+  //         name,
+  //         email
+  //       } )
+  //     } )
+  //     .then( function ( response ) {
+  //       return response.json()
+  //     } )
+  //     .then( function ( object ) {
+  //       document.body.innerHTML = object[ "id" ]
+  //     } )
+  //     .catch( function ( error ) {
+  //       document.body.innerHTML = error.message
+  //     } )
+  // }
